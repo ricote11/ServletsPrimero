c981607f1e19eb76e7f6622777539889ea833f22
Ricote <danielricomomp@gmail.com>
2021-10-26 21:42:27 +0200

    ultimo servlet del ejercicio

--- src/main/java/carpetaServlet/UsoServlets.java
+++ src/main/java/carpetaServlet/UsoServlets.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import principal.Principal;
+
 /**
  * Servlet implementation class UsoServlets
  */
 @WebServlet("/UsoServlets")
 public class UsoServlets extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private static Logger logger = LogManager.getLogger(Principal.class);
     /**
      * Default constructor. 
      */
@@ -26,7 +31,30 @@ public class UsoServlets extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String parameter = request.getParameter("table");
+		if(parameter!=null)
+		{
+			if(parameter.equals("departamento"))
+			{
+				request.getRequestDispatcher("/MostrarDepartamentosServ").forward(request, response);
+				logger.info("redireccionando a la consulta de departamentos");
+				
+			}
+			if(parameter.equals("empleado"))
+			{
+				
+				request.getRequestDispatcher("/MostrarEmpleadosServ").forward(request, response);
+				logger.info("redireccionando a la consulta de empleados");
+			}
+			
+			
+			
+		}
+		else {
+			logger.info("ningún parametro permitido");
+			
+			
+		}
 	}
 
 	/**
